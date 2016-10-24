febb3007f1e5dbe397ebf884be1745b24a008f10
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2016-10-24 13:30:17 +0100

    new script to update node-sass

--- 60node-sass.sh
+++ 60node-sass.sh
@@ -1,4 +1,49 @@
 #!/bin/bash
+#source env variables including node version
 . /opt/elasticbeanstalk/env.vars
-node node_modules/node-sass/scripts/install.js
-npm rebuild node-sass
+
+function error_exit
+{
+  eventHelper.py --msg "$1" --severity ERROR
+  exit $2
+}
+
+#redirect all output to cfn-init to capture it by log.io
+exec >>/var/log/cfn-init.log  2>&1
+#download and extract desired node.js version
+echo "checking node..."
+OUT=$( [ ! -d "/opt/elasticbeanstalk/node-install" ] && echo "trying to install node.js $NODE_VER"   && mkdir /opt/elasticbeanstalk/node-install ; cd /opt/elasticbeanstalk/node-install/ && \
+  wget -nc http://nodejs.org/dist/v$NODE_VER/node-v$NODE_VER-linux-$ARCH.tar.gz && \
+  tar --skip-old-files -xzpf node-v$NODE_VER-linux-$ARCH.tar.gz) || error_exit "Failed to UPDATE node version. $OUT" $?.
+echo $OUT
+
+
+#UNCOMMENT to update npm, otherwise will be updated on instance init or rebuild
+#rm -f /opt/elasticbeanstalk/node-install/npm_updated
+
+#make sure node binaries can be found globally
+if [ ! -L /usr/bin/node ]; then
+  ln -s /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/node /usr/bin/node
+fi
+
+if [ ! -L /usr/bin/npm ]; then
+ln -s /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm /usr/bin/npm
+fi
+
+echo "checking npm..."
+if [ ! -f "/opt/elasticbeanstalk/node-install/npm_updated" ]; then
+cd /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/ && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm update npm -g
+touch /opt/elasticbeanstalk/node-install/npm_updated
+echo "YAY! Updated global NPM version to `npm -v`"
+else
+  echo "Skipping NPM -g version update. To update, please uncomment 40install_node.sh:12"
+fi
+
+echo "recreate node-sass bindings"
+if [ ! -f "/opt/elasticbeanstalk/node-install/npm_updated" ]; then
+  /usr/bin/node node_modules/node-sass/scripts/install.js
+  /usr/bin/npm rebuild node-sass
+else 
+ echo "unable to update node-sass bindings"
+fi
+
